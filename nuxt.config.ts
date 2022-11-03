17e1727f2e9242147ab4f87e84b027a1c6dd123f
ML <me.mlaure@gmail.com>
2022-11-03 22:16:37 +0100

    disable `reactivityTransform`; remove pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,10 @@ export default defineNuxtConfig({
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
-    '@pinia/nuxt',
     '@nuxtjs/color-mode',
   ],
   experimental: {
-    reactivityTransform: true,
+    reactivityTransform: false,
     inlineSSRStyles: false,
   },
   css: [
