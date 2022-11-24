a110d430ff3388d987fe9588b70654d4e79db925
ML <me.mlaure@gmail.com>
2022-11-24 01:43:21 +0100

    add overlay comp

---
+++ components/BlurryOverlay.vue
@@ -0,0 +1,17 @@
+<template>
+  <Transition name="fade">
+    <div
+      v-show="navIsOpen"
+      class="nav-overlay"
+      tabindex="-100"
+      aria-hidden="true"
+    />
+  </Transition>
+</template>
+
+<style lang="scss" scoped>
+.nav-overlay {
+  @apply fixed h-full w-full backdrop-filter overflow-hidden
+    z-16 bg-black/50 top-0 left-0 backdrop-blur-0.088rem;
+}
+</style>
