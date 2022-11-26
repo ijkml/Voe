0a2dad014ffc487bd56ae5efa0492d72707caac9
ML <me.mlaure@gmail.com>
2022-11-26 14:42:29 +0100

    add extra elements for scrolling

--- pages/other.vue
+++ pages/other.vue
@@ -8,6 +8,12 @@ const router = useRouter();
       <div class="i-carbon-warning inline-block" />
     </div>
     <div>Other Page</div>
+    <div class="test space-y-8">
+      <section class="h-150vh bg-blue-300"></section>
+      <section class="h-150vh bg-white"></section>
+      <section class="h-150vh bg-blue-800"></section>
+      <section class="h-150vh bg-blue-500"></section>
+    </div>
     <div>
       <button class="btn text-sm m-3 mt-8" @click="router.back()">Back</button>
     </div>
