71538568519247d3de8979120b7adff9d41a3353
ML <me.mlaure@gmail.com>
2022-12-18 00:55:07 +0100

    dev: home

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import LoadingSvg from '@icons/loading.svg?component';
+const isSearching = ref(false);
 
+function searchAction() {
+  if (isSearching.value) {
+    return;
+  }
+
+  isSearching.value = true;
+  setTimeout(() => {
+    isSearching.value = false;
+  }, 3581);
+}
 </script>
 
 <template>
@@ -7,15 +19,23 @@
     <section class="hero">
       <h1 class="screamer">Fly with peace of mind</h1>
     </section>
-    <section class="ze-section search-overlap">
+    <section id="ze-search-overlap">
       <div>
+        <h2>Find your flight</h2>
         <form>
-          <div class="info-inputs">
-            <VInput
-              label="Departure Airport"
-              :appearance="{ active: 'hsl(263, 60%, 35%)' }"
-            />
+          <div class="info-input-cont">
+            <VInput class="info-input" label="Departure Airport" />
+            <VInput class="info-input" label="Arrival Airport" />
           </div>
+          <button
+            type="submit"
+            class="search-button"
+            :class="{ loading: isSearching }"
+            @click.prevent="searchAction"
+          >
+            <span>Search</span>
+            <LoadingSvg class="icon" />
+          </button>
         </form>
       </div>
     </section>
@@ -50,15 +70,67 @@
   font-family: Venturis;
 }
 
-.search-overlap {
-  @apply relative;
+#ze-search-overlap {
+  @apply relative mx-auto p-4 md:(p-8) lg:(max-w-screen-lg);
 
   > div {
-    @apply bg-white rounded-lg shadow-xl border w-full mt--30;
+    @apply bg-white rounded-lg mt--30 mb-8 p-4 w-full
+      sm:(p-9) light:(shadow-lg sm:shadow-xl shadow-zinc)
+        dark:(bg-dark-7 shadow-none border border-zinc-1/8);
   }
 
   form {
-    @apply w-40vw my-0 mx-auto p-12;
+    @apply md:(flex items-center gap-4);
+  }
+
+  .info-input-cont {
+    @apply grid gap-4 overflow-hidden p-1 flex-grow grid-cols-1;
+
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+
+    @media (min-width: 400px) {
+      grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    }
+  }
+
+  .info-input {
+    @apply w-full;
+  }
+
+  h2 {
+    @apply text-1.32rem sm:(text-2xl) mb-4;
+
+    font-family: Venturis;
+  }
+
+  .search-button {
+    @apply px-6 h-10 flex items-center transition duration-300
+      rounded-md bg-brand-pri text-white focus:outline-none
+        font-medium mt-4 sm:(h-11) md:(mt-0) relative;
+
+    &:hover, &:focus-visible {
+      @apply outline-none bg-opacity-80 dark:(bg-opacity-50);
+    }
+
+    > span {
+      @apply transition;
+    }
+
+    .icon {
+      @apply invisible transition absolute top-1/2 left-1/2
+        transform translate--1/2;
+    }
+
+    &.loading {
+      @apply cursor-wait;
+
+      > span {
+        @apply invisible;
+      }
+      .icon {
+        @apply visible;
+      }
+    }
   }
 }
 </style>
