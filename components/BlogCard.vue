11fd0805765ff03e75360a921df3db4b51a770c2
ML <me.mlaure@gmail.com>
2023-01-03 22:29:08 +0100

    refactor blog section

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -4,11 +4,12 @@ import type { BlogCard } from '@/types';
 const props = defineProps<BlogCard>();
 
 const { date, image, link, text, title } = toRefs(props);
+
 const imageUrl = `url(${image.value})`;
 </script>
 
 <template>
-  <NuxtLink :to="link" class="blog-card">
+  <NuxtLink v-lazy-img :to="link" class="blog-card">
     <div>
       <h3 v-text="title" />
       <div class="dated" v-text="date" />
@@ -18,7 +19,74 @@ const imageUrl = `url(${image.value})`;
 </template>
 
 <style scoped lang="scss">
-.blog-card {
+$line-no: 3;
+$pad-top: 1rem;
+$line-ht: 1.3rem;
+$elem-ht: ($line-ht * $line-no) + $pad-top;
+$overlay: hsla(263, 60%, 20%, 0.088);
+
+.img-y {
   --card-image: v-bind('imageUrl');
+}
+
+.blog-card {
+  @apply flex justify-center overflow-hidden rounded-xl items-end
+    transition-all duration-400 min-h-80 ease-in-out text-light-1
+      relative focus:outline-none bg-brand-pri-dark max-w-90 mx-auto
+        md:(max-w-100) ring-(1 zinc-5/25);
+
+  &::after {
+    @apply w-full h-full top-0 left-0 absolute z-0 transition-inherit
+      rounded-inherit filter content-[''];
+
+    background: linear-gradient($overlay, $overlay),
+      #210123 var(--card-image) bottom center / cover no-repeat;
+  }
+
+  > div {
+    @apply z-1 relative w-full bg-black/70 p-4 rounded-b-inherit
+      transition-inherit;
+
+    margin-bottom: -$elem-ht;
+  }
+}
+
+h3 {
+  @apply text-lg font-semibold;
+}
+
+.dated {
+  @apply text-xs font-mono block mt-2 text-zinc-2;
+}
+
+.peekaboo {
+  font-size: 0.88rem;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: $line-no;
+  line-clamp: $line-no;
+  padding-top: $pad-top;
+  line-height: $line-ht;
+  min-height: $elem-ht;
+
+  @apply overflow-hidden invisible transition max-w-50ch
+    duration-500 transform translate-y-full;
+}
+
+.blog-card {
+  &:hover,
+  &:focus-visible {
+    &::after {
+      @apply sepia-75 brightness-40;
+    }
+
+    > div {
+      @apply mb-0;
+    }
+
+    .peekaboo {
+      @apply delay-150 visible translate-y-0;
+    }
+  }
 }
 </style>
--- components/BlogSection.vue
+++ components/BlogSection.vue
@@ -9,74 +9,8 @@ import blog from '@/i18n/copy/blog';
 </template>
 
 <style scoped lang="scss">
-$line-no: 3;
-$pad-top: 1rem;
-$line-ht: 1.3rem;
-$elem-ht: ($line-ht * $line-no) + $pad-top;
-$overlay: hsla(263, 60%, 20%, 0.088);
-
 .top-cont {
   @apply grid grid-cols-1 gap-6 md:gap-8;
-}
-
-:deep(.blog-card) {
-  @apply flex justify-center overflow-hidden rounded-xl items-end
-    transition-all duration-400 min-h-80 ease-in-out text-light-1
-      relative focus:outline-none bg-brand-pri-dark max-w-90 mx-auto
-        md:(max-w-100) ring-(1 zinc-5/25);
-
-  &::after {
-    @apply w-full h-full top-0 left-0 absolute z-0 transition-inherit
-      rounded-inherit filter;
-
-    content: '';
-    background: linear-gradient($overlay, $overlay),
-      #210123 var(--card-image) bottom center / cover no-repeat;
-  }
-
-  > div {
-    @apply z-1 relative w-full bg-black/70 p-4 rounded-b-inherit
-      transition-inherit;
-
-    margin-bottom: -$elem-ht;
-  }
-
-  h3 {
-    @apply text-lg font-semibold;
-  }
-
-  .dated {
-    @apply text-xs font-mono block mt-2 text-zinc-2;
-  }
-
-  .peekaboo {
-    font-size: 0.88rem;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: $line-no;
-    line-clamp: $line-no;
-    padding-top: $pad-top;
-    line-height: $line-ht;
-    min-height: $elem-ht;
-
-    @apply overflow-hidden invisible transition max-w-50ch
-      duration-500 transform translate-y-full;
-  }
-
-  &:hover,
-  &:focus-visible {
-    &::after {
-      @apply sepia-75 brightness-40;
-    }
-
-    > div {
-      @apply mb-0;
-    }
-
-    .peekaboo {
-      @apply delay-150 visible translate-y-0;
-    }
-  }
 }
 
 @media (min-width: 400px) and (max-width: 519.9px) {
