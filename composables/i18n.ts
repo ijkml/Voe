bc6e8e7afb485766f56823504541dda3929c82d7
ML <me.mlaure@gmail.com>
2023-01-17 18:04:50 +0100

    fix support for urls with query and/or hash

--- composables/i18n.ts
+++ composables/i18n.ts
@@ -5,7 +5,13 @@ const availableLocales: LocaleCode[] = ['en', 'es', 'fr', 'ja', 'zh'];
 
 function removeLocaleCode(inputString: string) {
   const regex = new RegExp(`^/(${availableLocales.join('|')})`, 'i');
-  return inputString.replace(regex, '');
+  const plain = inputString.replace(regex, '');
+  return plain.startsWith('?') || plain.startsWith('#') ? `/${plain}` : plain;
+}
+
+function changeLocale(inputString: string, code: LocaleCode) {
+  const regex = new RegExp(`^/(${availableLocales.join('|')})`, 'i');
+  return inputString.replace(regex, `/${code}`);
 }
 
 const localizeUrl = (route: RouteLocationRaw, locale?: string | undefined) =>
@@ -13,4 +19,7 @@ const localizeUrl = (route: RouteLocationRaw, locale?: string | undefined) =>
 
 // const localizeUrl = useLocaleRoute();
 
-export { removeLocaleCode, localizeUrl };
+const localeHome = availableLocales.map((l) => `/${l}`);
+localeHome.push('/');
+
+export { removeLocaleCode, localizeUrl, changeLocale, localeHome };
