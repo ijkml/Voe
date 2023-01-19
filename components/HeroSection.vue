30682b66d4ff0106692115421b500b0833729770
ML <me.mlaure@gmail.com>
2023-01-19 18:00:50 +0100

    exp: really change hero img

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -3,7 +3,7 @@
     class="ze-hero"
     aria-labelledby="screamer"
     style="
-      --bg-urls: url(https://images.unsplash.com/photo-1559023234-1e773470544f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8YWlycG9ydHx8fHx8fDE2NzQxNDczMDA&ixlib=rb-4.0.3&q=80&w=1500);
+      --bg-url: url(https://images.unsplash.com/photo-1559023234-1e773470544f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8YWlycG9ydHx8fHx8fDE2NzQxNDczMDA&ixlib=rb-4.0.3&q=80&w=1500);
     "
   >
     <h1 id="screamer" class="font-serif" v-text="$t('hero')" />
